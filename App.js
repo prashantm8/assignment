7c9b72acdfec956848db60d359a6ae5f6f58eb23
prashantm8 <adpmishra818@gmail.com>
2019-08-16 01:59:00 +0530

    removed

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import {store} from './src/redux/store/store'
 import Feedback from './src/screens/feedback/index'
@@ -12,11 +11,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
